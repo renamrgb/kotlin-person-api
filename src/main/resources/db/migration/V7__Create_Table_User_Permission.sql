7d81bd744a27338b6c564b92e502f126db093e7a
Renam Bulhoes <renamgustavo@live.com>
2023-01-29 18:05:54 -0300

    Population security db

---
+++ src/main/resources/db/migration/V7__Create_Table_User_Permission.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE user_permission (
+  id_permission BIGINT NOT NULL,
+  id_user BIGINT NOT NULL
+);
+
+ALTER TABLE user_permission ADD CONSTRAINT fk_useper_on_permission FOREIGN KEY (id_permission) REFERENCES permission (id);
+
+ALTER TABLE user_permission ADD CONSTRAINT fk_useper_on_user FOREIGN KEY (id_user) REFERENCES users (id);
